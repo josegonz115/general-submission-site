67644b879db30085ce15a6fd4d1dcde50df83e0c
Jose <josegonz115@gmail.com>
2024-09-30 05:49:36 -0700

    include www domain...

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,8 @@ app.add_middleware(
     allow_origins=[
         #'http://localhost:5173',
         'https://general-submission-site.onrender.com',
-        'https://web-drive-submission-api.online'
+        'https://web-drive-submission-api.online',
+        'https://www.web-drive-submission-api.online',  
     ],
     allow_credentials=True,
     allow_methods=["*"],
